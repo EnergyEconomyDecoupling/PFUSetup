63169db7920b68a12effc804bd4c47becaf57aaa
Matthew Heun <matthew.heun@me.com>
2023-10-31 16:45:40 -0400

    Working on reading pins.

--- R/pin_functions.R
+++ R/pin_functions.R
@@ -1,23 +1,25 @@
-#' Get pin strings
+#' Get pin version strings for a CL-PFU database release
 #'
 #' The CL-PFU database uses the `pins` package
 #' to store results.
-#' This function translates a CL-PFU database version number to
-#' a pin string for later retrieval.
+#' This function gives the pin version strings for CL-PFU database release.
 #'
 #' The return value is a named list where the values are the pin version strings
 #' and the name are either product names or pin version strings.
 #'
-#' @param version A number representing the version for which pin names should be returned.
-#' @param version_string A string, prefixed with "v" for the version of interest.
-#'                       Default is `paste0("v", version)`.
-#'                       The `version_string` is used internally.
+#' @param database_version A string, prefixed with "v" for the version of interest.
+#'                         Any number will be prefixed by "v" and converted to a string internally.
 #' @param versions_and_products_path The path to the "versions and products.xlsx" file
 #'                                   that contains a concordance table of versions
 #'                                   and pin names.
+#'                                   Default is `get_abs_paths()[["versions_and_products_path"]]`.
 #' @param version_table_tab The name of the `version_table` tab in the
 #'                          versions and products file.
 #'                          Default is "version_table".
+#' @param product_column The name of the product column in the version table.
+#'                       Default is "product".
+#' @param pin_name_column The name of the pin name column in the version table.
+#'                        Default is "pin_name".
 #'
 #' @return A named list of pin version strings.
 #'         Names are the various pin names or the product names.
@@ -26,26 +28,58 @@
 #' @export
 #'
 #' @examples
-#' pin_names(1.1)
-#' pin_names(version_string = "v1.1")
-pin_names <- function(version,
-                      version_string = paste0(version_prefix, version),
-                      versions_and_products_path = get_abs_paths()[["versions_and_products_path"]],
-                      version_table_tab = "version_table",
-                      product_column = "product",
-                      pin_name_column = "pin_name",
-                      version_prefix = "v") {
+#' pin_versions("v1.1")
+#' pin_versions(1.1)
+pin_versions <- function(database_version,
+                         versions_and_products_path = get_abs_paths()[["versions_and_products_path"]],
+                         version_table_tab = "version_table",
+                         product_column = "product",
+                         pin_name_column = "pin_name") {
+  if (is.numeric(database_version)) {
+    database_version <- paste0("v", database_version)
+  }
+
   # Get the version table.
   cols <- readxl::read_excel(path = versions_and_products_path, sheet = version_table_tab) |>
-    dplyr::select(tidyselect::any_of(c(product_column, pin_name_column, version_string)))
+    dplyr::select(tidyselect::any_of(c(product_column, pin_name_column, database_version)))
+
+  if (!(database_version %in% names(cols))) {
+    stop(paste("Unknown database version:", database_version))
+  }
 
   # Create an outgoing list containing version strings
   # named with product names and with pin names.
-  c(cols[[version_string]] |>
+  c(cols[[database_version]] |>
       magrittr::set_names(cols[[pin_name_column]]),
-    cols[[version_string]] |>
+    cols[[database_version]] |>
       magrittr::set_names(cols[[product_column]])) |>
     as.list()
 }
 
 
+#' Read a version of a pinned CL-PFU database product
+#'
+#' @param database_version A string, prefixed with "v" for the version of interest.
+#'                         Any number will be prefixed by "v" and converted to a string internally.
+#' @param pin_name The string name of the pin to be read. Can be the pin name or a product name. See examples.
+#' @param pipeline_releases_folder The path to the pipeline releases folder.
+#'                                 Default is `get_abs_paths()[["pipeline_releases_folder"]]`.
+#'
+#' @return The pinned object represented by the name and the version string.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' pin_versions(1.2)[["phi_vecs"]] |>
+#'   read_pin_version(pin_name = "phi_vecs") |>
+#'   head()
+#'}
+read_pin_version <- function(pin_version_string,
+                             pin_name,
+                             pipeline_releases_folder = get_abs_paths()[["pipeline_releases_folder"]]) {
+
+  pipeline_releases_folder |>
+    pins::board_folder(versioned = TRUE) |>
+    pins::pin_read(name = pin_name, version = pin_version_string)
+}
--- tests/testthat/test-pin_functions.R
+++ tests/testthat/test-pin_functions.R
@@ -1,21 +1,39 @@
-test_that("get_pin_names() works as expected", {
-  pin_versions <- pin_names(1.1)
+test_that("pin_versions() works as expected", {
 
-  expect_equal(pin_versions$psut, "20230618T131003Z-4c70f")
-  expect_equal(pin_versions$A, "20230618T131003Z-4c70f")
-  expect_equal(pin_versions$psut_usa, "20230618T130939Z-50f39")
-  expect_equal(pin_versions$B, "20230618T130939Z-50f39")
-  expect_equal(pin_versions$eta_fu_Y_eiou, "20230527T005907Z-350b0")
-  expect_equal(pin_versions$C, "20230527T005907Z-350b0")
+  testthat::skip_on_ci()
 
-  expect_equal(pin_versions$sector_agg_eta_fu, "20230619T051454Z-e0f57")
-  expect_equal(pin_versions$`Agg-A`, "20230619T051454Z-e0f57")
-  expect_equal(pin_versions$sector_agg_eta_fu_csv, "20230621T172335Z-aca06")
-  expect_equal(pin_versions$`Agg-B`, "20230621T172335Z-aca06")
-  expect_equal(pin_versions$agg_eta_pfu, "20230619T051304Z-f653c")
-  expect_equal(pin_versions$`Agg-C`, "20230619T051304Z-f653c")
-  expect_equal(pin_versions$agg_eta_pfu_csv, "20230621T172311Z-d4cbf")
-  expect_equal(pin_versions$`Agg-D`, "20230621T172311Z-d4cbf")
-  expect_equal(pin_versions$psut_re_all_chop_all_ds_all_gr_all, "20230621T124407Z-c5f3c")
-  expect_equal(pin_versions$`Agg-E`, "20230621T124407Z-c5f3c")
+  # Test the pathological case
+  expect_error(pin_versions("bogus"), "Unknown database version: bogus")
+
+  # Test a real case
+  pin_vs <- pin_versions(1.1)
+
+  expect_equal(pin_vs$psut, "20230618T131003Z-4c70f")
+  expect_equal(pin_vs$A, "20230618T131003Z-4c70f")
+  expect_equal(pin_vs$psut_usa, "20230618T130939Z-50f39")
+  expect_equal(pin_vs$B, "20230618T130939Z-50f39")
+  expect_equal(pin_vs$eta_fu_Y_eiou, "20230527T005907Z-350b0")
+  expect_equal(pin_vs$C, "20230527T005907Z-350b0")
+
+  expect_equal(pin_vs$sector_agg_eta_fu, "20230619T051454Z-e0f57")
+  expect_equal(pin_vs$`Agg-A`, "20230619T051454Z-e0f57")
+  expect_equal(pin_vs$sector_agg_eta_fu_csv, "20230621T172335Z-aca06")
+  expect_equal(pin_vs$`Agg-B`, "20230621T172335Z-aca06")
+  expect_equal(pin_vs$agg_eta_pfu, "20230619T051304Z-f653c")
+  expect_equal(pin_vs$`Agg-C`, "20230619T051304Z-f653c")
+  expect_equal(pin_vs$agg_eta_pfu_csv, "20230621T172311Z-d4cbf")
+  expect_equal(pin_vs$`Agg-D`, "20230621T172311Z-d4cbf")
+  expect_equal(pin_vs$psut_re_all_chop_all_ds_all_gr_all, "20230621T124407Z-c5f3c")
+  expect_equal(pin_vs$`Agg-E`, "20230621T124407Z-c5f3c")
+
+})
+
+
+test_that("read_pin_version() works as expected", {
+
+  testthat::skip_on_ci()
+
+  phi_vecs <- pin_versions(1.2)[["phi_vecs"]] |>
+    read_pin_version(pin_name = "phi_vecs")
+  expect_equal(names(phi_vecs), c("Country", "Year", "phi"))
 })
