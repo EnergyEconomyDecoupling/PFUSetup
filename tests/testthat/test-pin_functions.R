f421a99a48eedb934dfbca246b79f6b1e51d738d
Matthew Heun <matthew.heun@me.com>
2023-10-31 13:16:30 -0400

    Tests for pin versions.

--- tests/testthat/test-pin_functions.R
+++ tests/testthat/test-pin_functions.R
@@ -1,5 +1,11 @@
 test_that("get_pin_names() works as expected", {
   pin_versions <- pin_names(1.1)
 
-  expect_equal()
+  expect_equal(pin_versions$psut, "20230618T131003Z-4c70f")
+  expect_equal(pin_versions$A, "20230618T131003Z-4c70f")
+  expect_equal(pin_versions$psut_usa, "20230618T130939Z-50f39")
+  expect_equal(pin_versions$B, "20230618T130939Z-50f39")
+  expect_equal(pin_versions$eta_fu_Y_eiou, "20230527T005907Z-350b0")
+  expect_equal(pin_versions$C, "20230527T005907Z-350b0")
+
 })
