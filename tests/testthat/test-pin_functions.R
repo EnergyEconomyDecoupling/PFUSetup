27da30b493da87adae3b2265973b286ab7350b43
Matthew Heun <matthew.heun@me.com>
2023-11-10 07:57:22 -0500

    Better tests

--- tests/testthat/test-pin_functions.R
+++ tests/testthat/test-pin_functions.R
@@ -17,6 +17,19 @@ test_that("pin_versions() works as expected", {
   expect_equal(pin_vs$agg_eta_pfu, "20230619T051304Z-f653c")
   expect_equal(pin_vs$agg_eta_pfu_csv, "20230621T172311Z-d4cbf")
   expect_equal(pin_vs$psut_re_all_chop_all_ds_all_gr_all, "20230621T124407Z-c5f3c")
+
+  # Test with a string
+  pin_vs_string <- pin_versions("v1.1")
+
+  expect_equal(pin_vs_string$psut, "20230618T131003Z-4c70f")
+  expect_equal(pin_vs_string$psut_usa, "20230618T130939Z-50f39")
+  expect_equal(pin_vs_string$eta_fu_Y_eiou, "20230527T005907Z-350b0")
+
+  expect_equal(pin_vs_string$sector_agg_eta_fu, "20230619T051454Z-e0f57")
+  expect_equal(pin_vs_string$sector_agg_eta_fu_csv, "20230621T172335Z-aca06")
+  expect_equal(pin_vs_string$agg_eta_pfu, "20230619T051304Z-f653c")
+  expect_equal(pin_vs_string$agg_eta_pfu_csv, "20230621T172311Z-d4cbf")
+  expect_equal(pin_vs_string$psut_re_all_chop_all_ds_all_gr_all, "20230621T124407Z-c5f3c")
 })
 
 
@@ -25,5 +38,8 @@ test_that("read_pin_version() works as expected", {
   testthat::skip_on_ci()
 
   phi_vecs <- read_pin_version(pin_name = "phi_vecs", database_version = 1.2)
+  expect_equal(names(phi_vecs), c("Country", "Year", "phi"))
+
+  phi_vecs_string <- read_pin_version(pin_name = "phi_vecs", database_version = "v1.2")
   expect_equal(names(phi_vecs), c("Country", "Year", "phi"))
 })
